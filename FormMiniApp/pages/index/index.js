3e8c6e9347a28262fe390baf051e5cac1b242196
Xingyu Xu <xingyxu@microsoft.com>
2018-10-29 22:05:18 +0800

    Add charts

--- FormMiniApp/pages/index/index.js
+++ FormMiniApp/pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hello World3',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
