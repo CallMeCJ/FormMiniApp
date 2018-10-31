3928329f23fa172f24499ab7cd9fae5d1020fa0c
ChaoJie Zheng <chzhe@microsoft.com>
2018-10-31 16:17:30 +0800

    finish quiz

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -3,28 +3,25 @@ const util = require('../../utils/util.js')
 const wxCharts = require('../../utils/wxcharts.js');
 var totalAverageScore = null;
 var mathSubjectsScore = null;
-var jackAverageScore = null;
-var roseAverageScore = null;
+var jackAndRoseRanking = null;
 var app = getApp();
 
 Page({
   data: {
     averageScore: {
       mathModuls: ['数的运算', '初步代数', '应用题', '量的计算', '空间与图形', '统计与概率'],
-      mathModulsScore: [95, 90, 80, 83, 75, 85],
+      mathModulsScore: [95, 90, 80, 83, 70, 85],
       timeCategories: ['2017-09', '2017-10', '2017-11', '2017-12', '2018-01', '2018-02', '2018-03', '2018-04', '2018-05', '2018-06'],
       averageScoresOfMath: [80.3, 81.4, 84.2, 70.1, 84.8, 86.6, 74.2, 85.9, 87.6, 87.8],
       averageScoresOfEnglish: [70.1, 73.3, 75.5, 79.4, 80.2, 81.8, 83.6, 85.0, 84.7, 85.6],
-      jackAverageScoresOfMath: [95, 96, 99, 99, 81, 72, 70, 65, 63, 62],
-      jackAverageScoresOfEnglish: [88, 87, 90, 91, 80, 81, 82, 75, 72, 70],
-      roseAverageScoresOfMath: [92, 94, 97, 98, 79, 70, 67, 60, 59, 55],
-      roseAverageScoresOfEnglish: [90, 95, 94, 90, 83, 79, 75, 75, 72, 70],
+      jackRanking: [2, 3, 2, 10, 20, 25, 26, 30, 15, 7],
+      roseRanking: [5, 4, 5, 20, 25, 27, 28, 29, 16, 6],
+      comments: [1, 2, 1, 1, 1, 5, 10, 20, 30, 30],
     }
   },
   onLoad: function (e) {
     this.onLoadAverageScore(e);
-    this.onLoadJackScore(e);
-    this.onLoadRoseScore(e);
+    this.onLoadJackAndRoseRanking(e);
     this.onLoadMathModulsScore(e);
   },
 
@@ -56,7 +53,7 @@ Page({
     });
   },
   
-  onLoadJackScore(e){
+  onLoadJackAndRoseRanking(e){
     var windowWidth = 320;
     try {
       var res = wx.getSystemInfoSync();
@@ -65,63 +62,27 @@ Page({
       console.error('getSystemInfoSync failed!');
     }
 
-    jackAverageScore = new wxCharts({
-      canvasId: 'jackAverageScoreLine',
+    jackAndRoseRanking = new wxCharts({
+      canvasId: 'rankingCanvas',
       type: 'line',
       categories: this.data.averageScore.timeCategories,
       animation: true,
       background: '#f5f5f5',
       series: [{
-        name: '数学',
-        data: this.data.averageScore.jackAverageScoresOfMath
+        name: '梁山伯',
+        data: this.data.averageScore.jackRanking
       }, {
-        name: '英语',
-        data: this.data.averageScore.jackAverageScoresOfEnglish
+        name: '朱丽叶',
+        data: this.data.averageScore.roseRanking
+      }, {
+        name: 'comments数',
+        data: this.data.averageScore.comments
       }],
       xAxis: {
         disableGrid: true
       },
       yAxis: {
-        title: '罗密欧平均分(百分制)',
-        min: 50
-      },
-      width: windowWidth,
-      height: 200,
-      dataLabel: false,
-      dataPointShape: true,
-      extra: {
-        lineStyle: 'curve'
-      }
-    });
-  },
-
-  onLoadRoseScore(e) {
-    var windowWidth = 320;
-    try {
-      var res = wx.getSystemInfoSync();
-      windowWidth = res.windowWidth;
-    } catch (e) {
-      console.error('getSystemInfoSync failed!');
-    }
-
-    roseAverageScore = new wxCharts({
-      canvasId: 'roseAverageScoreLine',
-      type: 'line',
-      categories: this.data.averageScore.timeCategories,
-      animation: true,
-      background: '#f5f5f5',
-      series: [{
-        name: '数学',
-        data: this.data.averageScore.roseAverageScoresOfMath
-      }, {
-        name: '英语',
-        data: this.data.averageScore.roseAverageScoresOfEnglish
-      }],
-      xAxis: {
-        disableGrid: true
-      },
-      yAxis: {
-        title: '祝英台平均分(百分制)',
+        title: '班级排名(35人)',
         min: 50
       },
       width: windowWidth,
@@ -188,5 +149,13 @@ Page({
         return category + ' ' + item.name + ':' + item.data
       }
     });
+  },
+  touchRankingHandler: function (e) {
+    jackAndRoseRanking.showToolTip(e, {
+      format: function (item, category) {
+        return category + ' ' + item.name + ':' + item.data
+      }
+    });
   }
+
 })
